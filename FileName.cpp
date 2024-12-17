ee86d64793cde43c33fa6181fa72d6284a57c0ca
papugaxx <tc10762@gmail.com>
2024-12-17 11:24:34 +0200

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -1,4 +1,6 @@
 #include "MyString.h"
+#include <iostream>
+using namespace std
 
 int main() {
     MyString defaultStr;
@@ -16,6 +18,20 @@ int main() {
     MyString copiedStr = initializedStr;
     cout << "copied str: " << copiedStr.getStr() << endl;
     cout << "size: " << copiedStr.getSize() << endl;
+
+    MyString anotherStr;
+    anotherStr.MyStrcpy(initializedStr);
+    cout << "anotherStr after copy: " << anotherStr.getStr() << endl;
+
+    cout << "strlen:" << initializedStr.MyStrLen() << endl;
+
+    char symbol;
+    cout << "symbol to search: ";
+    cin >> symbol;
+
+    int index = initializedStr.MyChr(symbol);
+    cout << index << endl;
+  
 
     return 0;
 }
